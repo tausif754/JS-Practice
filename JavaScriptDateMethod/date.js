ae5a87dbc05eb1572cda7d59738f616096c57c4f
Tausif Ahmad <tausifahmad754@gmail.com>
2024-02-01 15:27:28 +0530

    Learn JavaScript Date method

---
+++ JavaScriptDateMethod/date.js
@@ -0,0 +1,6 @@
+let now = new Date();
+// document.write(now);
+console.log(now.toDateString()); //Thu Feb 01 2024
+console.log(now.getFullYear()); //2024
+console.log(now.getMonth() + 1);
+console.log(now.getSeconds());
