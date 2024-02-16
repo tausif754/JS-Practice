d891a3e2c014e77d62fdded8f59ca44f08dec736
Tausif Ahmad <tausifahmad754@gmail.com>
2024-02-16 22:02:24 +0530

    setTimeout

--- Promise.js
+++ Promise.js
@@ -1,12 +1,22 @@
 // let complete = false;
-function promise(complete) {
+function createPromise(complete) {
   return (promise = new Promise(function (resolve, reject) {
-    if (complete) {
-      resolve("I am successfull");
-    } else {
-      reject("I am failed");
-    }
+    console.log("fetching data plase wait");
+    setTimeout(() => {
+      if (complete) {
+        resolve("I am successfull");
+      } else {
+        reject("I am failed");
+      }
+    }, 1000);
   }));
 }
+// console.log(promise(true));
+let onFulfilment = (result) => {
+  console.log(result);
+};
 
-console.log(promise(true));
+let onRejection = (error) => {
+  console.log(error);
+};
+createPromise(true).then(onFulfilment).catch(onRejection);
